37c4f6edabaa64f3bf212538fbcb26e4a72f218b
WibawaArif <arifwb.work@gmail.com>
2022-07-03 13:28:22 +0700

    first commit

--- movie_library/__init__.py
+++ movie_library/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 def create_app():
-    app = Flask(__name__,template_folder="templates", static_folder="static")
+    app = Flask(__name__)
     app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
     app.config["SECRET_KEY"] = os.environ.get(
         "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
